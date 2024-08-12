b2ef7b72eab1473b54e41e24d9b58d4d7f144f3e
VIKAS PAWAR <155407261+vikasp07@users.noreply.github.com>
2024-08-12 23:22:42 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'wanderlust_DEV',
+      folder: 'Wander_lust',
       allowedFormats: ["png", "jpg", "jpeg"],
     },
   });
